d399cef0b76b722866f059260cfde89855391cff
Emilyb261 <ebillings@unomaha.edu>
2025-09-28 10:25:09 -0500

    Update WordGame.py

--- WordGame.py
+++ WordGame.py
@@ -17,7 +17,18 @@ def rateGuess(myGuess, word):
     - Capital letter if the letter is in the right spot
     - Lower case letter if the letter is in the word but in the wrong spot
     - * if the letter is not in the word at all"""
+    result = ""
 
+    for i in range(len(myGuess)):
+     letter= myGuess[i]
+    if inSpot(letter, word, i):
+     result += letter.upper()
+    elif inWord(letter, word):    
+      result += letter.lower()
+    else:
+        result =+ "*"
+
+    return result 
 
 def main():
     #Pick a random word from the list of all words
@@ -28,13 +39,26 @@ def main():
     print(todayWord)
 
     #User should get 6 guesses to guess
+    for attempt in range(1, 7):
+        guess = input(f"Guess {attempt}/6: ").strip().lower()
+
+        # check length match
+        if len(guess) != len(todayWord):
+            print(f"Your guess must be {len(todayWord)} letters long.")
+            continue
+
+        feedback = rateGuess(guess, todayWord)
+        print("Feedback:", feedback)
+
+        if guess == todayWord:
+            print(f" You guessed it in {attempt} tries!")
+            break
+    else:
+        print("Out of guesses :( The word was:", todayWord)
+
 
     #Ask user for their guess
     #Give feedback using on their word:
-
-
-
-
 
 if __name__ == '__main__':
   main()
